6489469f2572ebdc835e8df7255d01fcb3958782
Aniket Kumar <92014181+aniket-devop@users.noreply.github.com>
2026-08-30 11:47:41 +0530

    Fix UI API endpoint

--- axion-ui/src/App.tsx
+++ axion-ui/src/App.tsx
@@ -15,7 +15,7 @@ import { SystemSettings } from './components/pages/SystemSettings';
 import { DashboardView } from './components/pages/DashboardView';
 import { NotFound } from './components/pages/NotFound';
 
-const API_BASE = 'https://api.axionsystems.de';
+const API_BASE = 'http://20.75.174.83:8000';
 
 function App() {
   const location = useLocation();
--- axion-ui/src/components/pages/DashboardView.tsx
+++ axion-ui/src/components/pages/DashboardView.tsx
@@ -7,7 +7,7 @@ import { Throughput } from '../Throughput';
 import { DigitalTwin } from '../DigitalTwin';
 import { ThermalCamera } from '../ThermalCamera';
 
-const API_BASE = 'https://api.axionsystems.de';
+const API_BASE = 'http://20.75.174.83:8000';
 
 interface DashboardViewProps {
   devices: any[];
